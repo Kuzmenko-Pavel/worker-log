3251cd4855bd5f64d314af6bba13c3fde3a3b27f
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-01-30 16:32:26 +0200

    library dependency fix

--- DEBUG.sh
+++ DEBUG.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #tar zxvf install/mongodb-fix.tar.gz -C /
-#apt-get install automake libtool libtinyxml2-dev libtinyxml-dev
+#apt-get install automake libtool libtinyxml2-dev libtinyxml-dev g++ libgeoip-dev
 git log --oneline --decorate --color > ChangeLog
-git log --format='%aN <%aE>' | sort -f | uniq > AUTHORS
 libtoolize --install --copy --force --automake
 aclocal -I m4 --install
 autoconf
