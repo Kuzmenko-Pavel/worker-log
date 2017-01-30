031f69b0ee6df55eddc2b566a2be72f768b27231
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2017-01-30 14:49:40 +0200

    --

--- DEBUG.sh
+++ DEBUG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-tar zxvf install/mongodb-fix.tar.gz -C /
-apt-get install automake libtool libtinyxml2-dev libtinyxml-dev
+#tar zxvf install/mongodb-fix.tar.gz -C /
+#apt-get install automake libtool libtinyxml2-dev libtinyxml-dev
 git log --oneline --decorate --color > ChangeLog
 git log --format='%aN <%aE>' | sort -f | uniq > AUTHORS
 libtoolize --install --copy --force --automake
